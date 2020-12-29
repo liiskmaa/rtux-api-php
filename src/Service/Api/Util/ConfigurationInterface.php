deba4819384370133f85afd3821c5ba6488f9b2b
danneg <dana@boxalino.com>
2020-12-29 23:48:59 +0100

    [configuration] constants

--- src/Service/Api/Util/ConfigurationInterface.php
+++ src/Service/Api/Util/ConfigurationInterface.php
@@ -9,6 +9,13 @@ namespace Boxalino\RealTimeUserExperienceApi\Service\Api\Util;
  */
 interface ConfigurationInterface
 {
+    public const RTUX_API_ENDPOINT_PRODUCTION="https://main.bx-cloud.com/narrative/%%account%%/api/1";
+    public const RTUX_API_ENDPOINT_STAGE="https://r-st.bx-cloud.com/narrative/%%account%%/api/1";
+    public const BOXALINO_API_TRACKING_PRODUCTION="//track.bx-cloud.com/static/bav2.min.js";
+    public const BOXALINO_API_TRACKING_STAGE="//r-st.bx-cloud.com/static/bav2.min.js";
+    public const BOXALINO_API_SERVER_PRODUCTION="//track.bx-cloud.com/track/v2";
+    public const BOXALINO_API_SERVER_STAGE="//r-st.bx-cloud.com/track/v2";
+    
     /**
      * The API endpoint depends on the testing conditionals and on the data index
      *
